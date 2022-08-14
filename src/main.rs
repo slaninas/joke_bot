8112a67b6c8ebe1083c37ebddecc6b3951a77b13
slaninas <slaninas@pm.me>
2022-08-14 11:29:43 +0200

    Add code and repo commands + cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use nostr_bot::{get_reply, Command, Event, EventNonSigned, FunctorType, State};
-
-use std::time::SystemTime;
+use nostr_bot::{
+    get_reply, keypair_from_secret, wrap_state, Command, Event, EventNonSigned, FunctorType};
 
 struct Info {
     started_at: std::time::SystemTime,
     jokes_told: u64,
 }
+
+type State = nostr_bot::State<Info>;
 
 async fn fetch_joke(category: &str) -> Result<String, reqwest::Error> {
     let url = format!("https://v2.jokeapi.dev/joke/{}?format=txt", category);
@@ -14,7 +15,7 @@ async fn fetch_joke(category: &str) -> Result<String, reqwest::Error> {
     response.text().await
 }
 
-async fn joke(event: Event, state: State<Info>) -> EventNonSigned {
+async fn joke(event: Event, state: State) -> EventNonSigned {
     let args = event.content.split_whitespace().collect::<Vec<_>>();
     let category = if args.len() > 1 {
         match args[1] {
@@ -43,9 +44,9 @@ async fn joke(event: Event, state: State<Info>) -> EventNonSigned {
     get_reply(event, response)
 }
 
-async fn stats(event: Event, state: State<Info>) -> EventNonSigned {
+async fn stats(event: Event, state: State) -> EventNonSigned {
     let state = state.lock().await;
-    let uptime_seconds = SystemTime::now()
+    let uptime_seconds = std::time::SystemTime::now()
         .duration_since(state.started_at)
         .unwrap()
         .as_secs();
@@ -59,11 +60,12 @@ async fn stats(event: Event, state: State<Info>) -> EventNonSigned {
     )
 }
 
-async fn code(event: Event, _state: State<Info>) -> EventNonSigned {
-    get_reply(
-        event,
-        String::from("You can see the source code at https://github.com/slaninas/joke_bot."),
-    )
+async fn code(event: Event, _state: State) -> EventNonSigned {
+    get_reply(event, String::from(std::include_str!("main.rs")))
+}
+
+async fn repo(event: Event, _state: State) -> EventNonSigned {
+    get_reply(event, String::from("https://github.com/slaninas/joke_bot"))
 }
 
 #[tokio::main]
@@ -79,16 +81,16 @@ async fn main() {
     let mut secret = std::fs::read_to_string("secret").unwrap();
     secret.pop(); // Remove newline
 
-    let keypair = nostr_bot::keypair_from_secret(&secret);
+    let keypair = keypair_from_secret(&secret);
 
     let pic_url = "https://media.istockphoto.com/vectors/yellow-emoticons-and-emojis-vector-id1179177852?k=20&m=1179177852&s=612x612&w=0&h=5UrMXuV5x2WCI-d8twGI9gPZ6810wpgqyF9oy7X-r9M=";
 
-    let state = nostr_bot::wrap_state(Info {
-        started_at: SystemTime::now(),
+    let state = wrap_state(Info {
+        started_at: std::time::SystemTime::now(),
         jokes_told: 0,
     });
 
-    let mut bot = nostr_bot::Bot::<State<Info>>::new(keypair, relays, state)
+    let mut bot = nostr_bot::Bot::new(keypair, relays, state)
         .name("joke_bot")
         .about("Just joking around. Blame https://sv443.net/jokeapi/v2/ if you don't like a joke.")
         .picture(pic_url)
@@ -98,7 +100,8 @@ async fn main() {
             Command::new("!stats", nostr_bot::wrap!(stats))
                 .description("Shows for how long I'm running and how many jokes I told."),
         )
-        .command(Command::new("!code", nostr_bot::wrap!(code)).description("Gives link to bot's source code."))
+        .command(Command::new("!code", nostr_bot::wrap!(code)).description("Spits out bot's source code."))
+        .command(Command::new("!repo", nostr_bot::wrap!(repo)).description("Gives link to bot's repository."))
         .help();
 
     bot.run().await;
